898fa510d86317909006a69dcd623237aef3f3be
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-22 17:14:50 -0400

    Validation: * Update Validate Keys - add check for special characters

--- program_io/validate/keys.py
+++ program_io/validate/keys.py
@@ -1,13 +1,26 @@
 """ Key Validation """
 
+
 def isValid(key: str) -> bool:
-    """ Determines if the String is a valid Key """
+    """
+    Determines if the String is a valid Key.
+    If Key is invalid, a message is printed containing details
+    """
+
     # Check for Empty String
     if key == None or len(key) < 1:
         print("Key Cannot be Empty")
         return False
+    
     # Limit Key Length
     elif len(key) > 1000:
         print("Key Is Too Large")
         return False
+    
+    # Reserved and inappropriate characters
+    elif key in ('*', '_', '"'):
+        print("Illegal Key : " + key)
+        return False
+    
+    # Key is Valid
     return True
