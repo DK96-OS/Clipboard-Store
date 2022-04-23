49b45be51c516b60c886fd44ef8cc886d523e007
DK96 <69859316+dk96-os@users.noreply.github.com>
2022-04-23 16:11:52 -0400

    Delete Command: * Implement Delete Command - enable multiple key arguments to be passed

--- command/delete.py
+++ command/delete.py
@@ -1,16 +1,45 @@
 """ The Delete Command """
 
 
-def run() -> bool:
+def run(args: list) -> bool:
     """
+    Validates Delete Command Arguments
+    Initiates Delete Command Operation
+    Returns True if a Key was deleted
     """
+    # Ensure that args is not empty
+    if len(args) < 1:
+        return False
+    # Each Argument is a Key to be deleted
+    # Load Data File once
+    from program_io.files import data_file
+    data = data_file.load()
+    # Data needs to be saved only if a Key is deleted
+    data_updated = False
     #
-    print("Delete Command Unimplemented")
-    return _cmd_del()
+    for key in args:
+        # Check if the Delete Operation succeeds
+        if _cmd_del(data, key):
+            data_updated = True
+            print("Successfully Deleted Key (" + key + ")")
+    # 
+    if data_updated:
+        # Save the updated Data File
+        data_file.save(data)
+        return True
+    # If no Keys were deleted
+    return False
 
 
-def _cmd_del() -> bool:
+def _cmd_del(data: dict, key: str) -> bool:
     """
+    Performs the Delete Command Operation
+    Returns True if the Key was deleted
     """
-    #
-    return True
+    # Check if Key exists
+    if key in data:
+        del data[key]
+        # Key Deleted, notify caller
+        return True
+    # Key did not exist, no changes
+    return False
